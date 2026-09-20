3d918dc6397dc7e70813f2e6d06d8d7882256ba2
kurihara <takaokuriharagomes@gmail.com>
2026-09-19 21:42:10 -0300

    sistema restaurante

---
+++ src/SistemaDeRestaurante/Cliente.java
@@ -0,0 +1,25 @@
+package SistemaDeRestaurante;
+
+public class Cliente {
+
+    String nome;
+    double saldo;
+    int quantidadePedidos;
+
+    public Cliente(String nome, double saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+        quantidadePedidos = 0;
+    }
+
+    void realizarPagamento(Pratos pratos) {
+        if (pratos.preco > 0 && saldo > pratos.preco) {
+            saldo -= pratos.preco;
+        }
+    }
+
+    void exibirDados() {
+        System.out.println("");
+    }
+
+}
